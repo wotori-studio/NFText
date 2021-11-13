acbd1e6518fb26af489b7e4ea75179e2e547003b
Wotori Movako <10486621+wotori@users.noreply.github.com>
2021-11-13 10:02:34 +0300

    Initial commit

---
+++ pages/about.jsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Link from "../src/Link";
+
+export default function About() {
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          NFText page
+        </Typography>
+        <Button variant="contained" component={Link} noLinkStyle href="/">
+          Go back
+        </Button>
+      </Box>
+    </Container>
+  );
+}
