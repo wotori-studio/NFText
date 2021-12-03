cade59e2169b33f4a341bd5f38cb49e42f6be62a
Dmitry <dmsobolev@yahoo.com>
2021-12-03 15:35:50 +0300

    nft load base logic added

--- pages/api/nft/query.js
+++ pages/api/nft/query.js
@@ -1,4 +1,4 @@
-let toen_uri_data = {
+let token_uri_data = {
   name: "TheCat",
   description: "what a wonderful cat ...",
   image: "ipfs://QmWTvKbPeWDwNQMFgbmWzciq7NNcwsQcBgV5jLNNhPNwF6",
@@ -7,9 +7,10 @@ let toen_uri_data = {
 
 export default function handler(req, res) {
   res.status(200).json({
+    // this return should be directly from Archway network
     data: {
       token_uri: "ipfs://QmbSvPGYVaQ8m5fvFcPRefoFkrZ4sctytwX3xhojjUDw7B",
-      token_uri_data: toen_uri_data, // TODO: query data from ipfs
+      token_uri_data: token_uri_data, // TODO: query data from ipfs. This key is not come from Archway network
       extension: null,
     },
   });
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,34 @@
-import * as React from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Link from "../src/Link";
 import CubeScene from "../src/3D/cube";
-import Image from 'next/image'
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function NftLoad() {
+  const [img, setImg] = useState(null);
+
+  useEffect(() => {
+    axios.get("/api/nft/query").then((response) => {
+      let data = response.data.data;
+      let url = data.token_uri_data.image.slice(7);
+      let convertedUrl = `https://ipfs.io/ipfs/${url}`;
+      console.log(convertedUrl);
+      setImg(convertedUrl);
+    });
+  }, []);
+
+  return <img src={img} />;
+}
 
 export default function Index() {
   return (
     <Container maxWidth="sm">
-      <img src="https://ipfs.io/ipfs/QmWTvKbPeWDwNQMFgbmWzciq7NNcwsQcBgV5jLNNhPNwF6" />
+      <NftLoad />
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
-          NFText It!
+          Create Collaborative NFT on Cosmos Archway!
         </Typography>
         <Container sx={{ height: 500 }}>
           <CubeScene />
