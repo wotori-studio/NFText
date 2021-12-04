b5652ccfcc6a65eaa0b2d71792e7938ee48cb285
Dmitry <dmsobolev@yahoo.com>
2021-12-04 08:19:29 +0300

    fetch and axios for post request implementation

--- pages/mint/index.jsx
+++ pages/mint/index.jsx
@@ -1,18 +1,34 @@
 import { Button, Input } from "@mui/material";
+import axios from "axios";
 import React, { Component, useState } from "react";
 
 const index = () => {
   const [selectedFile, setSelectedFile] = useState("");
-  const [selected, setSelected] = useState(false);
+  const [isSelected, setSelected] = useState(false);
 
   const changeHandler = (e) => {
     const file = e.target.files[0];
     console.log(file);
-    setSelectedFile(file)
+    setSelectedFile(file);
     setSelected(true);
   };
 
-  const handleSubmission = () => {};
+  const handleSubmission = () => {
+    const formData = new FormData();
+
+    formData.append("File", selectedFile);
+
+    // axios.post("/api/pinata/post", { data: "data" }).then((resp) => {
+    //   console.log("response data: ", resp);
+    // });
+
+    fetch("/api/pinata/post", {
+      method: "POST",
+      body: formData,
+    }).then((resp) => {
+      console.log(resp);
+    });
+  };
 
   return (
     <div>
