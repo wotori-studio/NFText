b58bff2d1ce33bb4a93335d6404de7e7aaf151ab
Wotori Movako <10486621+wotori@users.noreply.github.com>
2021-12-04 22:39:46 +0300

    style

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
 import theme from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
+import '../styles.css'
 
 const clientSideEmotionCache = createEmotionCache();
 
