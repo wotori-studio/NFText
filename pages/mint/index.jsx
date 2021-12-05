88fb4e7bca193a4b5cd4c96c340ca3957d5059fb
Wotori Movako <10486621+wotori@users.noreply.github.com>
2021-12-06 00:09:15 +0300

    Milestone #1 (#1)
    * import styles TODO
    
    * refactor
    
    * menu_states_first_iter
    
    * logic improvements
    
    * yahoo, toggleMode done
    
    * display uploaded image
    
    * fix before lifting state up
    
    * prepared for lifting up
    
    * lifted up toggle-mode component
    
    * toggle menu, hide interface
    
    * fixture
    
    * base interface template
    
    * prepare before integrating composition model into Uploder component
    
    * refactored with composition
    
    * fixtures
    
    * todo + readme

--- pages/mint/index.jsx
+++ pages/mint/index.jsx
@@ -1,17 +1,90 @@
-import Uploader from "./interface";
+import Uploader from "/src/components/uploader/main";
+import { useState } from "react";
 
 const index = () => {
+  /* TODO: import style as modules https://nextjs.org/blog/styling-next-with-styled-jsx */
+
+  const [curMode, setCurMode] = useState("");
+  const [textMode, setTextMode] = useState(false);
+  const [paintMode, setPaintMode] = useState(false);
+  const [imgMode, setImgMode] = useState(false);
+  const [gltfMode, setGltfMode] = useState(false);
+
+  let modes = [
+    ["text", textMode],
+    ["paint", paintMode],
+    ["img", imgMode],
+    ["gltf", gltfMode],
+  ];
+
+  const handleClick = (item) => {
+    setCurMode(item[0]);
+
+    if (item[0] === "gltf") {
+      setTextMode(false);
+      setImgMode(false);
+      setGltfMode(true);
+      setPaintMode(false);
+    }
+    if (item[0] === "text") {
+      setTextMode(true);
+      setImgMode(false);
+      setGltfMode(false);
+      setPaintMode(false);
+    }
+    if (item[0] === "img") {
+      setTextMode(false);
+      setImgMode(true);
+      setGltfMode(false);
+      setPaintMode(false);
+    }
+    if (item[0] === "paint") {
+      setTextMode(false);
+      setImgMode(false);
+      setGltfMode(false);
+      setPaintMode(true);
+    }
+  };
+
   return (
     <>
       <style jsx>
         {`
-          .WTF {
-            margin: 50px;
+          .flexy {
+            display: flex;
+            justify-content: space-between;
+          }
+          .div-main {
+            padding-top: 100px;
+            padding-left: 550px;
+            padding-right: 550px;
+          }
+          .div-menu {
+            padding-bottom: 50px;
           }
         `}
       </style>
-      <div className="WTF">
-        <Uploader />
+      <div className="div-main">
+        <div className="div-menu">
+          <div className="flexy">
+            {modes.map((item) => {
+              return (
+                <div>
+                  <button
+                    className={item[1] ? "custom_btn" : "custom_btn_not_active"}
+                    onClick={() => handleClick(item)}
+                  >
+                    {item[0]}
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="uploader">
+          <Uploader mode={curMode} />
+        </div>
+        {/* based on Cosmos Archway project. */}
       </div>
     </>
   );
