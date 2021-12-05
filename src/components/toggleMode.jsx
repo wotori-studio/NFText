bd5b7ac7f9b92e11f91d5f81b2d50871ed9b310b
Wotori <wotorimovako@gmail.com>
2021-12-05 16:38:40 +0300

    logic improvements

--- src/components/toggleMode.jsx
+++ src/components/toggleMode.jsx
@@ -1,24 +1,34 @@
+import { useState } from "react";
 import ButtonState from "./button_2";
 
 export default function ToggleMode() {
+  const [activeText, setActiveText] = useState(false);
+  const [activeImg, setActiveImg] = useState(false);
+  const [activeGlts, setActiveGlts] = useState(false);
+
+  let buttons = [
+    ["text", activeText],
+    ["img", activeImg],
+    ["gltf", activeGlts],
+  ];
+
   const handleClick = (e) => {
     console.log(e);
-    buttons = [
-        ["text", false],
-        ["img", false],
-        ["gltf", false],
-      ];
+    // setActiveText(true);
+    // setActiveImg(true);
+    // setActiveGlts(true);
   };
-  let buttons = [
-    ["text", true],
-    ["img", false],
-    ["gltf", false],
-  ];
+
   return (
     <>
       {buttons.map((item) => {
         return (
-          <ButtonState onClick={handleClick} text={item[0]} active={item[1]} />
+          <button
+            className={item[1] ? "custom_btn" : "custom_btn_not_active"}
+            onClick={() => handleClick(item)}
+          >
+            {item[0]}
+          </button>
         );
       })}
     </>
