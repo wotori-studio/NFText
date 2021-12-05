34fd160ff432b80b92f8a0c1128f764a3684e22b
Wotori <wotorimovako@gmail.com>
2021-12-05 22:50:42 +0300

    base interface template

--- pages/mint/index.jsx
+++ pages/mint/index.jsx
@@ -5,11 +5,13 @@ const index = () => {
   /* TODO: import style as modules https://nextjs.org/blog/styling-next-with-styled-jsx */
 
   const [textMode, setTextMode] = useState(false);
+  const [paintMode, setPaintMode] = useState(false);
   const [imgMode, setImgMode] = useState(false);
   const [gltfMode, setGltfMode] = useState(false);
 
   let modes = [
     ["text", textMode],
+    ["paint", paintMode],
     ["img", imgMode],
     ["gltf", gltfMode],
   ];
@@ -19,16 +21,25 @@ const index = () => {
       setTextMode(false);
       setImgMode(false);
       setGltfMode(true);
+      setPaintMode(false);
     }
     if (item[0] === "text") {
       setTextMode(true);
       setImgMode(false);
       setGltfMode(false);
+      setPaintMode(false);
     }
     if (item[0] === "img") {
       setTextMode(false);
       setImgMode(true);
       setGltfMode(false);
+      setPaintMode(false);
+    }
+    if (item[0] === "paint") {
+      setTextMode(false);
+      setImgMode(false);
+      setGltfMode(false);
+      setPaintMode(true);
     }
   };
 
@@ -50,6 +61,9 @@ const index = () => {
           }
           .uploader-img {
             display: ${imgMode ? true : "none"};
+          }
+          .uploader-paint {
+            display: ${paintMode ? true : "none"};
           }
           .uploader-text {
             display: ${textMode ? true : "none"};
@@ -79,8 +93,9 @@ const index = () => {
         <div className="uploader-img">
           <Uploader />
         </div>
-        <div className="uploader-text">TEXT</div>
-        <div className="uploader-gltf">3D</div>
+        <div className="uploader-text">text</div>
+        <div className="uploader-paint">paint</div>
+        <div className="uploader-gltf">3d</div>
         {/* based on Cosmos Archway project. */}
       </div>
     </>
