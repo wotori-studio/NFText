401e7f2ed4d788dc02e3cca699b8b91b619b852e
Vitaliy Malyshev <zich.contemporary@mail.ru>
2022-03-22 23:43:05 -0600

    Complete: model uploader (.glb, .gltf)

--- src/components/3D/Loader.jsx
+++
@@ -1,7 +0,0 @@
-import { useLoader } from "@react-three/fiber";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-
-export default function ThreeFileLoader() {
-  const gltf = useLoader(GLTFLoader, "/models/monkey.glb");
-  return <primitive object={gltf.scene} />;
-}
