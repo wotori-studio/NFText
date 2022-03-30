47ed33c732359738b5b4059fe0656dab6fb3c01f
Wotori <10486621+wotori@users.noreply.github.com>
2022-03-31 00:25:47 +0300

    improvements and test render

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import ModelViewer from "../../src/components/ModelViewer";
 import NFImage from "../../src/components/NFImage/NFImage";
 import NFText from "../../src/components/NFText/NFText";
+import { Nft } from "../../src/models/Nft";
 
 const PUBLIC_CW721_CONTRACT = process.env
   .NEXT_PUBLIC_APP_CW721_CONTRACT as string;
@@ -13,9 +14,9 @@ const rpcEndpoint = process.env.NEXT_PUBLIC_CHAIN_RPC_ENDPOINT as string;
 const NFTID = () => {
   const router = useRouter();
   const { id } = router.query;
-  const token_id = id;
+  const token_id = id?.toString();
   const [client, setClient] = useState<CosmWasmClient>();
-  const [nft, setNft] = useState();
+  const [nft, setNft] = useState<Nft>();
 
   useEffect(() => {
     const getClient = async () => {
@@ -33,22 +34,40 @@ const NFTID = () => {
       console.log("start query smart-contract");
       client
         .queryContractSmart(PUBLIC_CW721_CONTRACT, {
-          all_nft_info: { token_id: token_id + "" },
+          all_nft_info: { token_id: token_id },
         })
         .then((meta) => {
           console.log("meta:", meta);
           const decodedMetadata = JSON.parse(
             Buffer.from(meta.info.token_uri.slice(30), "base64").toString()
           );
-          setNft(decodedMetadata);
+
+          const newNFT: Nft = {
+            id: parseInt(token_id),
+            owner: meta.access.owner,
+            name: decodedMetadata.title,
+            type: decodedMetadata.type,
+            href: `/items/${token_id + 1}`,
+            content:
+              decodedMetadata.content || "https://dummyimage.com/404x404",
+          };
+          setNft(newNFT);
           console.log("decoded meta", decodedMetadata);
         });
     }
-  }, [token_id]);
+  }, [token_id, client]);
 
   return (
     <div>
-      <div>page {token_id}</div>
+      {nft ? (
+        <>
+          {nft.type === "text" && <NFText NFT={nft} />}
+
+          {nft.type === "img" && <NFImage NFT={nft} />}
+
+          {nft.type === "3d" && <ModelViewer NFT={nft} />}
+        </>
+      ) : null}
     </div>
   );
 };
