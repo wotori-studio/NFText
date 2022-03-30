eab808c5f7b482c24aec2e553ad097fc35688851
Wotori <10486621+wotori@users.noreply.github.com>
2022-03-31 00:49:09 +0300

    proxy added

--- next.config.js
+++ next.config.js
@@ -1,11 +1,25 @@
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
 
-module.exports = withPlugins([
-  // [optimizedImages, {
-    /* config for next-optimized-images */
-  // }],
+// module.exports = withPlugins([
+// [optimizedImages, {
+/* config for next-optimized-images */
+// }],
 
-  // your other plugins here
+// your other plugins here
 
-]);
+// ]);
+
+// https://nextjs.org/docs/migrating/incremental-adoption#micro-frontends-with-monorepos-and-subdomains
+module.exports = {
+  async rewrites() {
+    return {
+      fallback: [
+        {
+          source: "/:path*",
+          destination: `https://nftext.wotori.com/:path*`,
+        },
+      ],
+    };
+  },
+};
