c77c268f7c4d835af191d67168b3da27497f8d3a
Wotori <10486621+wotori@users.noreply.github.com>
2022-06-07 00:00:50 +0300

    return null from loader if status loading === 100

--- src/components/r3f/Loader.jsx
+++ src/components/r3f/Loader.jsx
@@ -2,5 +2,5 @@ import { Html, useProgress } from "@react-three/drei";
 
 export default function Loader() {
   const { progress } = useProgress();
-  return progress !== 100 && <Html center>{progress} % loaded</Html>;
+  return progress !== 100 ? <Html center>{progress} % loaded</Html> : null;
 }
