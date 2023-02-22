110365825b2754bedc40874462b5edf1e44a9cef
Wotori Movako <10486621+wotori@users.noreply.github.com>
2023-02-22 23:44:22 +0300

    add wotori-api env url

--- src/components/SayHi.tsx
+++ src/components/SayHi.tsx
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
 
+const WOTORI_API_URL = process.env.NEXT_PUBLIC_APP_WOTORI_API || "";
+
 export default function SayHi() {
   useEffect(() => {
+    console.log("Say Hi!", WOTORI_API_URL);
     const isProduction = process.env.NODE_ENV === "production";
-    let url = "https://wotori-api.herokuapp.com/hi";
     if (isProduction) {
-      fetch(url, { method: "GET" });
+      fetch(WOTORI_API_URL, { method: "GET" });
     }
   }, []);
 
