134fdcc34f13a3c186f8ff37b3a547323655d477
Wotori Movako <10486621+wotori@users.noreply.github.com>
2023-02-23 00:48:49 +0300

    hardcoded wotori-api path for sayhi

--- src/components/SayHi.tsx
+++ src/components/SayHi.tsx
@@ -1,13 +1,10 @@
 import { useEffect } from "react";
 
-const WOTORI_API_URL = process.env.NEXT_PUBLIC_APP_WOTORI_API || "";
-
 export default function SayHi() {
   useEffect(() => {
-    console.log("Say Hi!", WOTORI_API_URL);
     const isProduction = process.env.NODE_ENV === "production";
     if (isProduction) {
-      fetch(WOTORI_API_URL, { method: "GET" });
+      fetch("https://ekza.space/hi/", { method: "GET" });
     }
   }, []);
 
