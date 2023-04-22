573666d3bf6f44177b84cf8f1b41a7025b76df8a
Wotori Movako <10486621+wotori@users.noreply.github.com>
2023-04-22 21:31:53 +0300

    successifully mint with smart contract that instantiated with the factory

--- src/components/devTests/testContract/test.jsx
+++ src/components/devTests/testContract/test.jsx
@@ -1,30 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSigningClient } from "../../../context/cosmwasm";
 import { calculateFee } from "@cosmjs/stargate";
 
 export default function TestContract() {
   const { walletAddress, signingClient } = useSigningClient();
+  let [txHash, setTxHash] = useState(0);
+  let [newContract, setNewContract] = useState(0);
 
   function executeSmartContract() {
     console.log("test execute", signingClient);
+    let newSmartContractData = {
+      minter: walletAddress,
+      name: "Collection Name",
+      symbol: "Symbol URL",
+    };
+    const base64Str = btoa(JSON.stringify(newSmartContractData));
+    let instantiateMessage = {
+      instantiate_stored_contract: {
+        code_id: 633,
+        admin: walletAddress,
+        init_msg: base64Str,
+        label: "test",
+      },
+    };
+    console.log("smart contract data: ", newSmartContractData, base64Str);
     signingClient
       ?.execute(
         walletAddress,
         "archway19nhk3a94lpvtwgp3z7fuz75jrkv2y5seuwrt883y9362jrz4w42qelsk6e",
-        {
-          instantiate_stored_contract: {
-            code_id: 633,
-            init_msg:
-              "eyJtaW50ZXIiOiJhcmNod2F5MXFxNjV3amVmdTZubnF4MG42dnZ4NXh6ejN4bWN1eTc1dmF1aHE5IiwgIm5hbWUiOiJ0ZXN0IiwgInN5bWJvbCI6InRlc3QifQ==",
-            admin: "archway1qq65wjefu6nnqx0n6vvx5xzz3xmcuy75vauhq9",
-            label: "test",
-          },
-        },
+        instantiateMessage,
         calculateFee(600_000, "20uconst")
       )
       .then((response) => {
+        let txHashResp = response.transactionHash;
+        alert(`Success! TxHash: ${txHashResp}`);
+        setTxHash(txHashResp);
         console.log(response);
-        alert("Success!", response.text);
       })
       .catch((error) => {
         alert("Error during minted.");
@@ -36,13 +47,33 @@ export default function TestContract() {
 
   function getAddress() {
     console.log("Getting address...");
-    signingClient.getTx(
-        "351D15952B166AA515A0E67028690AE685111DA0FD980C210599D0766BB179E1"
+    signingClient.getTx(txHash).then((response) => {
+      let data = JSON.parse(response.rawLog)[0].events;
+      let newContractAddress = data[1].attributes[0].value;
+      setNewContract(newContractAddress);
+      console.log("all data: ", data);
+      console.log("address: ", newContractAddress);
+      console.log(`Success! New contract: ${newContractAddress}`);
+    });
+  }
+
+  function mintNFT() {
+    console.log("Minting...");
+    signingClient
+      ?.execute(
+        walletAddress,
+        newContract,
+        {
+          mint: {
+            token_id: "1",
+            owner: `${walletAddress}`,
+            token_uri: `data:application/json;base64, test`,
+          },
+        },
+        calculateFee(600_000, "20uconst")
       )
       .then((response) => {
-        let data = JSON.parse(response.rawLog)[0].events
-        console.log(data);
-        alert("Success!", );
+        alert("Successfully minted!", response);
       });
   }
 
@@ -51,9 +82,19 @@ export default function TestContract() {
   }, [signingClient]);
 
   return (
-    <>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "flex-start",
+      }}
+    >
+      <p>txHash: {txHash}</p>
+      <p>new contract address: {newContract}</p>
       <button onClick={executeSmartContract}>execute</button>
       <button onClick={getAddress}>address</button>
-    </>
+      <button onClick={mintNFT}>mint</button>
+    </div>
   );
 }
